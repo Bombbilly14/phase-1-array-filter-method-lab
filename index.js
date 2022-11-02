8ac491f113b1e4844f9e6d3bfbec033359b84281
William German <william.j.german2@gmail.com>
2022-11-02 14:57:46 -0700

    commit tree 07a4887a6f2b297c72cbf1983df1a861583a352d

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching (drivers, searchName) {
+    
+    return drivers.filter(driver =>driver.toUpperCase() === searchName.toUpperCase())
+    // return drivers.filter(function(driver) {
+       // return driver.toUpperCase() === searchName.toUpperCas
+   // })
+}
+function fuzzyMatch(drivers, searchChars) {
+    
+    const charLength= searchChars.length
+    return drivers.filter(driver => driver.slice(0, charLength) === searchChars)
+    // "Bobby" === "Sa" 
+    // "Bobby".slice(0,2) => "Bo" === "Sa"
+
+}
+
+function matchName(drivers, hometown){
+
+    for (const user of drivers) {
+        if (user.name.hometown() === hometown) {
+            return user
+        }
+    }
+
+}
